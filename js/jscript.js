1bad90116d177ad0ba5b273dacf193114796bbfe
Volodymyr Tokaruk <vovatokaruk@gmail.com>
2018-07-14 00:52:51 +0300

    додав анімацій

--- js/jscript.js
+++ js/jscript.js
@@ -15,3 +15,35 @@ $(document).ready(function(){
 
 });
 /* nav bar */
+$(document).ready(function(){
+
+    var target = $(".my-skil").offset().top;
+
+    $(window).scroll(function(){
+        var scrollPos = $(window).scrollTop();
+        
+        if (scrollPos = target){
+            $(".my-skil").addClass("my-skil-ani");
+        } else {
+            $(".my-skil").removeClass("my-skil-ani");
+        }
+   
+    });
+});
+
+$(document).ready(function(){
+
+    var target = $(".html-css-js-logo").offset().top;
+
+    $(window).scroll(function(){
+        var scrollPos = $(window).scrollTop();
+        
+        if (scrollPos = target){
+            $(".html-css-js-logo").addClass("html-css-js-logo-anim");
+        } else {
+            $(".html-css-js-logo").removeClass("html-css-js-logo-anim");
+        }
+   
+    });
+});
+/* about me */
